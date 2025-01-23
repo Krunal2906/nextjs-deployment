85f05bfc99462510a499603e4a03e3b54e77599b
root <krunalshaikh786@gmail.com>
2025-01-24 01:43:34 +0530

    Test workflow after merge

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Welcome to My Next.js App!</h1>
-      <p>This is a production build served using Next.js1.</p>
+      <p>This is a production page build served using Next.js1.</p>
     </div>
   );
 }
