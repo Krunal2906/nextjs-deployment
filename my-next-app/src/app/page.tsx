bfdd18e33808bf81d4f4a5bd6758a0d02f66b8be
root <krunalshaikh786@gmail.com>
2025-01-27 16:07:52 +0530

    update

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Welcome to Next.js App!</h1>
-      <p>This is a new production page build served using Next.js</p>
+      <p>TThis is a new production page build served using Next.js</p>
     </div>
   );
 }
