c1309eb89070dbfb0777f7c5d235947ae35e8e3e
root <krunalshaikh786@gmail.com>
2025-01-28 12:48:04 +0530

    MY

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Welcome to Next.js App!</h1>
+      <h1>Welcome to my Next.js App!</h1>
       <p>This is a new production page build served using Next.js</p>
     </div>
   );
